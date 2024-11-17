1c5ddb71e88af7d14db8d861a32ccb0bb4695310
kkong7 <kkongk@naver.com>
2024-11-17 21:12:13 +0900

    ui2 -H

--- lib/item_list_page.dart
+++ lib/item_list_page.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:shop/constants.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shop/item_details_page.dart';
 
 import 'models/product.dart';
 
@@ -13,107 +13,120 @@ class ItemListPage extends StatefulWidget {
 }
 
 class _ItemListPageState extends State<ItemListPage> {
-  final NumberFormat numberFormat=NumberFormat("###,###,###");
-
-  List<Product> productLists=[
+  List<Product> productLists = [
     Product(
-      productNo:1,
-      productName:'노트북(LapTop)',
+      productNo: 1,
+      productName: '노트북(LapTop)',
       productImageUrl: 'https://picsum.photos/id/1/300/300',
-      price:600000,
+      price: 600000,
     ),
     Product(
-      productNo:2,
-      productName:'스마트폰(Phone)',
+      productNo: 2,
+      productName: '스마트폰(Phone)',
       productImageUrl: 'https://picsum.photos/id/20/300/300',
-      price:500000,
+      price: 500000,
     ),
     Product(
-      productNo:3,
-      productName:'머그컵(Cup)',
+      productNo: 3,
+      productName: '머그컵(Cup)',
       productImageUrl: 'https://picsum.photos/id/30/300/300',
-      price:15000,
+      price: 15000,
     ),
     Product(
-      productNo:4,
-      productName:'키보드(Keyboard)',
+      productNo: 4,
+      productName: '키보드(Keyboard)',
       productImageUrl: 'https://picsum.photos/id/60/300/300',
-      price:50000,
+      price: 50000,
     ),
     Product(
-      productNo:5,
-      productName:'포도(Grape)',
+      productNo: 5,
+      productName: '포도(Grape)',
       productImageUrl: 'https://picsum.photos/id/75/300/300',
-      price:75000,
+      price: 75000,
     ),
     Product(
-      productNo:6,
-      productName:'책(book)',
+      productNo: 6,
+      productName: '책(book)',
       productImageUrl: 'https://picsum.photos/id/24/300/300',
-      price:24000,
+      price: 24000,
     ),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:GridView.builder(
-        itemCount:productLists.length,
-        itemBuilder:(context,index){
+      body: GridView.builder(
+        itemCount: productLists.length,
+        itemBuilder: (context, index) {
           return productContainer(
-            productName:productLists[index].productName??'',
-            productImageUrl: productLists[index].productImageUrl??'',
-            price:productLists[index].price??0,
+            productNo: productLists[index].productNo ?? 0,
+            productName: productLists[index].productName ?? '',
+            productImageUrl: productLists[index].productImageUrl ?? '',
+            price: productLists[index].price ?? 0,
           );
         },
-        gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio:0.9,
+          childAspectRatio: 0.9,
         ),
       ),
     );
   }
-  Widget productContainer({
-    required String productName,
-    required String productImageUrl,
-    required double price
-  }){
-    return Container(
-      padding:const EdgeInsets.all(5),
-      child:Column(
-        children: [
-          CachedNetworkImage(
-            imageUrl: productImageUrl,
-            height: 150,
-            fit:BoxFit.cover,
-            placeholder:(context,url){
-              return const CircularProgressIndicator(
-                strokeWidth: 2,
-              );
-            },
-            errorWidget: (context,url,error){
-              return const Center(
-                child: Text('오류 발생'),
+
+  Widget productContainer(
+      {required int productNo,
+      required String productName,
+      required String productImageUrl,
+      required double price}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return ItemDetailsPage(
+                productNo: productNo,
+                productName: productName,
+                productImageUrl: productImageUrl,
+                price: price,
               );
             },
           ),
-          Container(
-            padding:const EdgeInsets.all(8),
-            child:Text(
-              productName,
-              style:const TextStyle(
-                fontWeight:FontWeight.bold,
+        );
+      },
+      child: Container(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl: productImageUrl,
+                height: 155,
+                fit: BoxFit.cover,
+                placeholder: (context, url) {
+                  return const CircularProgressIndicator(
+                    strokeWidth: 2,
+                  );
+                },
+                errorWidget: (context, url, error) {
+                  return const Center(
+                    child: Text('오류 발생'),
+                  );
+                },
               ),
-            ),
-          ),
-          Container(
-            padding:const EdgeInsets.all(8),
-            child: Text(
-              '${numberFormat.format(price)}원'
-            ),
-          )
-        ],
-      )
+              Container(
+                padding: const EdgeInsets.all(4),
+                child: Text(
+                  productName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(1),
+                child: Text('${numberFormat.format(price)}원'),
+              )
+            ],
+          )),
     );
   }
 }

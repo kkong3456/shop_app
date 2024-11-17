95dee172309470fff14deca915cc4c700aab4665
unknown <kkong3456@gmail.com>
2024-11-17 14:54:47 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key,required this.title}) : super(key: key);
+  final title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:Container(
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                'XXXX'
+              ),
+            ),
+            Container(
+              child: Text('KKK'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/models/product.dart
@@ -0,0 +1,6 @@
+class Product{
+  int? productId;
+  String? productName;
+
+  Product({required this.productId,required this.productName,});
+}
